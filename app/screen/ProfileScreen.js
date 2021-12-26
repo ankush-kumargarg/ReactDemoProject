570e789487ec19216587a89677d5f82542e114c2
Ankush Kumar <ankush@Ankushs-MacBook-Air.local>
2021-12-26 23:11:51 +0530

    Changes

--- app/screen/ProfileScreen.js
+++ app/screen/ProfileScreen.js
@@ -1,17 +1,10 @@
 import React from 'react';
-
 import { StyleSheet, Text, View, TouchableOpacity, Button, Image,Permission, PermissionsAndroid, Platform } from 'react-native';
-
 import * as ImagePicker from 'react-native-image-picker';
 
- 
-
 export default class App extends React.Component {
-
   constructor(props) {
-
     super(props);
-
     this.state = {
       resourcePath: {},
       permission:false
@@ -19,14 +12,34 @@ export default class App extends React.Component {
   }
 
  requestCameraPermission = async () => {
-     if(Platform.OS==='android'){
+    if (Platform.OS === 'android') {
+        PermissionsAndroid.requestMultiple(
+          [PermissionsAndroid.PERMISSIONS.CAMERA, 
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE]
+          ).then((result) => {
+            if ( result['android.permission.CAMERA']
+            && result['android.permission.READ_EXTERNAL_STORAGE']
+            && result['android.permission.WRITE_EXTERNAL_STORAGE'] === 'granted') {
+              this.setState({
+                permission: true
+              });
+            } else if (result['android.permission.CAMERA']
+            || result['android.permission.READ_EXTERNAL_STORAGE']
+            || result['android.permission.WRITE_EXTERNAL_STORAGE'] === 'never_ask_again') {
+              this.refs.toast.show('Please Go into Settings -> Applications -> APP_NAME -> Permissions and Allow permissions to continue');
+            }
+          });
+      }
+
+     /*if(Platform.OS==='android'){
         try {
             const granted = await PermissionsAndroid.request(
               PermissionsAndroid.PERMISSIONS.CAMERA,PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
               {
                 title: "Cool Photo App Camera Permission",
                 message:
-                  "Cool Photo App needs access to your camera " +
+                  "App needs access to your camera " +
                   "so you can take awesome pictures.",
                 buttonNeutral: "Ask Me Later",
                 buttonNegative: "Cancel",
@@ -43,71 +56,40 @@ export default class App extends React.Component {
           } catch (err) {
             console.warn(err);
           }
-     }
+     }*/
   };
 
   selectFile = () => {
-
     var options = {
-
       title: 'Select Image',
-
       customButtons: [
-
         { 
-
           name: 'customOptionKey', 
-
           title: 'Choose file from Custom Option' 
-
         },
-
       ],
-
       storageOptions: {
-
         skipBackup: true,
-
         path: 'images',
-
       },
-
     };
 
- 
-
     ImagePicker.showImagePicker(options, res => {
-
       console.log('Response = ', res);
-
       if (res.didCancel) {
-
         console.log('User cancelled image picker');
-
       } else if (res.error) {
-
         console.log('ImagePicker Error: ', res.error);
-
       } else if (res.customButton) {
-
         console.log('User tapped custom button: ', res.customButton);
-
         alert(res.customButton);
-
       } else {
-
         let source = res;
-
         this.setState({
-
           resourcePath: source,
-
         });
-
       }
-
     });
-
   };
 
  
@@ -115,235 +97,118 @@ export default class App extends React.Component {
   // Launch Camera
 
   cameraLaunch = () => {
-
     let options = {
-
       storageOptions: {
-
         skipBackup: true,
-
         path: 'images',
-
       },
-
     };
 
     ImagePicker.launchCamera(options, (res) => {
-
       console.log('Response = ', res);
-
- 
-
       if (res.didCancel) {
-
-        console.log('User cancelled image picker');
-
+       console.log('User cancelled image picker');
       } else if (res.error) {
-
         console.log('ImagePicker Error: ', res.error);
-
       } else if (res.customButton) {
-
         console.log('User tapped custom button: ', res.customButton);
-
         alert(res.customButton);
-
       } else {
-
         const source = { uri: res.uri };
-
         console.log('response', JSON.stringify(res));
-
         this.setState({
-
           filePath: res,
-
           fileData: res.data,
-
           fileUri: res.uri
-
         });
-
       }
-
     });
-
   }
-
- 
 
   imageGalleryLaunch = () => {
-
     let options = {
-
       storageOptions: {
-
         skipBackup: true,
-
         path: 'images',
-
       },
-
     };
 
- 
-
     ImagePicker.launchImageLibrary(options, (res) => {
-
       console.log('Response = ', res);
-
- 
-
       if (res.didCancel) {
-
         console.log('User cancelled image picker');
-
       } else if (res.error) {
-
         console.log('ImagePicker Error: ', res.error);
-
       } else if (res.customButton) {
-
         console.log('User tapped custom button: ', res.customButton);
-
         alert(res.customButton);
-
       } else {
-
         const source = { uri: res.uri };
-
         console.log('response', JSON.stringify(res));
-
         this.setState({
-
           filePath: res,
-
           fileData: res.data,
-
           fileUri: res.uri
-
         });
-
       }
-
-    });
-
+    })
   }  
 
- 
-
   render() {
-
     return (
-
       <View style={styles.container}>
-
         <View style={styles.container}>
-
           <Image
-
             source={{
-
               uri: 'data:image/jpeg;base64,' + this.state.resourcePath.data,
-
             }}
-
             style={{ width: 100, height: 100 }}
-
           />
-
           <Image
-
             source={{ uri: this.state.resourcePath.uri }}
-
             style={{ width: 200, height: 200 }}
-
           />
-
           <Text style={{ alignItems: 'center' }}>
-
             {this.state.resourcePath.uri}
-
           </Text>
-
- 
-
           <TouchableOpacity onPress={this.selectFile} style={styles.button}  >
-
               <Text style={styles.buttonText}>Select File</Text>
-
           </TouchableOpacity>
-
- 
-
           <TouchableOpacity onPress={this.state.permission ? this.cameraLaunch : this.requestCameraPermission} style={styles.button}  >
-
               <Text style={styles.buttonText}>Launch Camera Directly</Text>
-
           </TouchableOpacity>
-
- 
-
           <TouchableOpacity onPress={this.imageGalleryLaunch} style={styles.button}  >
-
               <Text style={styles.buttonText}>Launch Image Gallery Directly</Text>
-
           </TouchableOpacity>
-
         </View>
-
       </View>
-
     );
-
   }
-
 }
 
- 
-
 const styles = StyleSheet.create({
-
   container: {
-
     flex: 1,
-
     padding: 30,
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
     backgroundColor: '#fff'
-
   },
 
   button: {
-
     width: 250,
-
     height: 60,
-
     backgroundColor: '#3740ff',
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
     borderRadius: 4,
-
     marginBottom:12    
-
   },
 
   buttonText: {
-
     textAlign: 'center',
-
     fontSize: 15,
-
     color: '#fff'
-
   }
 
 });
